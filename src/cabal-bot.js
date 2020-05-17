754193abd70a9357d1c1f41dfdb244a3e3ee69ac
Tim Göller <tim.goeller@posteo.de>
2020-05-17 15:11:03 +0200

    add expression solving

--- src/cabal-bot.js
+++ src/cabal-bot.js
@@ -1,10 +1,56 @@
+var Client = require('cabal-client')
+var CabalBotExpression = require('./expression')
+
 class CabalBot {
   constructor(name, opts) {
+    if(!name) {
+      throw "name must be set"
+    }
+    opts = opts || {}
     this.name = name
-    this.symbol = opts.symbol ?? "!"
-    if(this.symbol.length == 1) {
+    this.symbol = opts.symbol === undefined ? "!" : opts.symbol
+    if(this.symbol.length != 1) {
       throw "symbol can't be longer than one character"
     }
     this.channels = opts.channels
+    this.client = new Client(opts.clientOpts)
+    this.messageListeners = []
+    this.expressions = []
+  }
+
+  joinCabal(key) {
+    let initTime = new Date().getTime()
+    this.client.addCabal(key).then(cabalDetails =>
+      {
+        cabalDetails.on("init", () => { 
+          cabalDetails.publishNick("[BOT] " + this.name, () => {})
+          cabalDetails.on("new-message", (envelope) => {
+            if(envelope.message.value.timestamp > initTime) {
+              this._onNewMessage(envelope, cabalDetails)
+            }
+          })
+        })
+      })
+  }
+
+  _onNewMessage(envelope, cabalDetails) {
+    if(this.channels) {
+      if(!this.channels.includes(envelope.channel)) return
+    }
+    if(envelope.message.value.content.text != "") {
+      if(envelope.message.value.content.text.charAt(0) == this.symbol) {
+        this.expressions.forEach(expr => {
+          expr._runExpression(cabalDetails, envelope)
+        });
+      }
+    }
+  }
+
+  expr() {
+    let expr = new CabalBotExpression()
+    this.expressions.push(expr)
+    return expr
   }
 }
+
+module.exports = CabalBot
---
+++ src/expression.js
@@ -0,0 +1,85 @@
+class CabalBotExpression {
+  constructor(firstExpr) {
+    this.firstExpr = firstExpr
+
+    if(!this.firstExpr) {
+      this.firstExpr = this
+    }
+  }
+
+  onCommand(commandName) {
+    if(!commandName) {
+      throw "name of command must be set"
+    }
+    this.resolve = this._resolveOnCommand(commandName)
+
+    this.nextExpr = new CabalBotExpression(this.firstExpr)
+    return this.nextExpr 
+  }
+
+  _resolveOnCommand(commandName) {
+    return (envelope, messageText, cabal) => {
+      let beforeWhitespace = messageText.match(/^([^\s]+)/g)
+      if(beforeWhitespace) {
+        beforeWhitespace = beforeWhitespace[0]
+      }
+      else return {match: false}
+
+      if(beforeWhitespace === commandName) {
+        let afterWhitespace = messageText.match(/\s[\s\S]*/g)
+        if(afterWhitespace) {
+          afterWhitespace = afterWhitespace[0].substring(1)
+        }
+        else afterWhitespace = ""
+        return {match: true, 
+          messageText: afterWhitespace}
+      }
+      else {
+        return {match: false}
+      }
+    }
+  }
+
+  inChannel(channelName) {
+    if(!channelName) {
+      throw "name of channel must be set"
+    }
+    this.resolve = this._resolveInChannel(channelName)
+
+    this.nextExpr = new CabalBotExpression(this.firstExpr)
+    return this.nextExpr 
+  }
+
+  _resolveInChannel(channelName) {
+    return (envelope, messageText, cabal) => {
+      if(envelope.channel === channelName) {
+        return {match: true, messageText: messageText}
+      }
+      else {
+        return {match: false}
+      }
+    }
+  }
+
+  thenDo(cb) {
+    this.cb = cb
+  }
+
+  _runExpression(cabal, envelope) {
+    let messageText = envelope.message.value.content.text.substring(1)
+    let currentExpression = this.firstExpr
+    while(true) {
+      if(!currentExpression.resolve) break
+      let resolveResult = currentExpression.resolve(envelope, messageText, cabal)
+      if(!resolveResult.match) return
+      messageText = resolveResult.messageText
+      currentExpression = currentExpression.nextExpr
+    }
+
+    if(currentExpression.cb) {
+      currentExpression.cb(messageText, cabal, envelope)
+    }
+  }
+}
+
+module.exports = CabalBotExpression
